e431a551709778afdc00736d3848d0a04fbc76c4
baligh-p <belighzoughlemi8@gmail.com>
2022-04-02 00:03:47 +0100

    version 1.0.1 completed nav design without fixing 2xl warning

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
 import { AdvertiseComponent } from './advertise/advertise.component';
+import { NavDropComponent } from './nav-drop/nav-drop.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AdvertiseComponent } from './advertise/advertise.component';
     NavComponent,
     LoginComponent,
     AdvertiseComponent,
+    NavDropComponent,
   ],
   imports: [
     BrowserModule,
