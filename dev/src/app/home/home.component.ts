4a38e55154ea51b80c5f5c452e8aa5dcefa4e030
baligh-p <belighzoughlemi8@gmail.com>
2022-04-15 12:02:11 +0100

    v0.0.1 just for local

--- dev/src/app/home/home.component.ts
+++ dev/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy } from '@angular/core';
 import { AppService } from '../app.service';
 
 @Component({
@@ -6,20 +6,26 @@ import { AppService } from '../app.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy  {
 
   constructor(private appService : AppService) { }
 
   ngOnInit(): void {
     this.getUsers()
   }
+  ngOnDestroy(): void {
+    clearInterval(this.interval)
+  }
   loadingForPosts=false
+  interval : any
   getUsers(){
-    this.loadingForPosts=true
-    this.appService.getData("/getPost.php?generalType=informatique").then((res)=>{
+    this.interval=setInterval(()=>{
+       this.loadingForPosts=true
+      this.appService.getData("/getPost.php?generalType=informatique").then((res)=>{
       this.usersData=res.data
       this.loadingForPosts=false
     })
+    },3000)
   }
   usersData=[]
 }
